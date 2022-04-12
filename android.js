f7d458a434ac4ecc99ffd74191751272dd8aa357
Lorenzo <techsharpii@gmail.com>
2022-04-12 12:10:28 +0200

    Update android.js

--- android.js
+++ android.js
@@ -1,12 +1,30 @@
+// if (window.innerWidth && window.innerWidth <= 600) {
+//     $(document).ready(function () {
+//         $('#header ul').addClass('nascondi');
+//         var bloccoDaInserire = $('<div class="bottone"> <input id="ppp" type="button" value="Show" onClick="toggleMenu()"></div>');
+//         bloccoDaInserire.appendTo('#header');
+//     });
+
+//     function toggleMenu() {
+//         if ($('ppp').attr('value') == 'Show') {
+//             $('#ppp').attr('value', "hide");
+//         }
+//         else {
+//             $('#ppp').attr('value', "Show");
+//         }
+//         $('#header ul').toggleClass('nascondi');
+//     }
+// }
+
 if (window.innerWidth && window.innerWidth <= 600) {
+    console.log("ok2");
     $(document).ready(function () {
         $('#header ul').addClass('nascondi');
-        var bloccoDaInserire = $(' <div class="bottone" > <input id="ppp" type="button" value="Show" onClick="toggleMenu()"></div>');
-        bloccoDaInserire.appendTo('#header');
+        var bloccoDaInserire = $('<div class="bottone"> <input id="ppp" type="button" value="Mostra" onclick="toggleMenu()"></div>');
+        bloccoDaInserire.appendTo("#header");
     });
-
     function toggleMenu() {
-        if ($('ppp').attr('value') == 'Show') {
+        if ($('#ppp').attr('value') == 'Show') {
             $('#ppp').attr('value', "hide");
         }
         else {
